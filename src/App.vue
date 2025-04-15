a073a7a31ae13a3be0224953a2764a174de93c7f
Milenakindleindarosa <milena.kindlein.da.rosa.ifc>
2025-04-15 11:59:29 -0300

    FEAT: Criação do Header Ainda em desenvolvimento

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
 
+import { ref, computed } from "vue";
+
 </script>
 
-<script src="https://kit.fontawesome.com/efc388a4ff.js" crossorigin="anonymous"></script>
+
 
 <template>
+
+  <header>
+    <div>
+      <img src="https://res.cloudinary.com/beleza-na-web/image/upload/f_svg,fl_progressive,q_auto:eco/v1/blz/assets-store/0.0.505/images/store/47/logo.svg" alt="Logo-oBoticário">
+      <div>
+        <form action="">
+
+          <p>
+            <textarea name="pesquisa" id="pesquisar">Pesquisar</textarea>
+            <input type="submit" value="">
+          </p>
+          
+        </form>
+      </div>
+    </div>
+  </header>
   
 </template>
 
