e7395d04643d6aa1f2c9546c712f3b0c8dc78837
Milenakindleindarosa <milena.kindlein.da.rosa.ifc>
2025-04-15 10:49:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<script src="https://kit.fontawesome.com/efc388a4ff.js" crossorigin="anonymous"></script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
