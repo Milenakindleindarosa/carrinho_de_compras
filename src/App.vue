63879e5d29c1307059383992e1fde6a8e86eba19
analaura3s <analauras.ifc.info3@gmail.com>
2025-04-25 15:05:29 -0300

    FEAT: botão e título.

--- src/App.vue
+++ src/App.vue
@@ -28,21 +28,23 @@ import { ref, computed } from "vue";
 
     <section class="sugestao">
 
+     <h2 class="sugestaoProduto">Sugestão de produto</h2>
+
       <ul>
 
-        <li>Sugestão de produto</li>
+        <li class="nome">Combo Cuide-se Bem Feira Cuidados Faciais: Gel de Limpeza Tangerina 150g + Hidratante Cremoso Banana 80g + Máscara Facial Noturna Melancia 50g</li>
 
-        <li>Combo Cuide-se Bem Feira Cuidados Faciais: Gel de Limpeza Tangerina 150g + Hidratante Cremoso Banana 80g + Máscara Facial Noturna Melancia 50g</li>
-
-        <li>Com Cuide-se Bem Feira, a feira é diferente, está em toda parte, vai com você aonde você for, no dia e na hora que você quiser! O Combo Cuide-se Bem Feira Cuidados Faciais proporciona uma mistura de frutas tropicais para um skincare com diferentes fragrâncias, texturas e novas maneiras de se amar.</li>
-
-        <li>R$111,90</li>
-
-        <li>ou 3x de R$37,30</li>
-
-        <li><img src="/images/sugestao.png" alt=""></li>
+        <li class="descricao">Com Cuide-se Bem Feira, a feira é diferente, está em toda parte, vai com você aonde você for, no dia e na hora que você quiser! O Combo Cuide-se Bem Feira Cuidados Faciais proporciona uma mistura de frutas tropicais para um skincare com diferentes fragrâncias, texturas e novas maneiras de se amar.</li>
 
       </ul>
+
+        <ul class="precoImagem">
+
+          <li class="preco">R$111,90<br>ou 3x de R$37,30</li>
+
+          <li><img src="/images/sugestao.png" alt=""></li>
+
+        </ul>
 
     </section>
 
@@ -53,5 +55,22 @@ import { ref, computed } from "vue";
 <style src="./assets/reset.css"></style>
 
 <style scoped>
+
+main section h2.sugestaoProduto{
+
+background-color: rgb(209, 186, 255);
+padding: 15px;
+margin: 5vw 82vw 0 4vw;
+font-size: 1.5rem;
+text-align: center;
+
+}
+
+main section ul li.nome{
+
+font-size: xx-large;
+margin: 3vw 9vw 3vw 2vw;
+
+}
 
 </style>
