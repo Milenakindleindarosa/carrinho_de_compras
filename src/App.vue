0c49689981dcd045c2b6e778996de244368ef5da
Milenakindleindarosa <milena.kindlein.da.rosa.ifc@gmail.com>
2025-05-05 22:28:56 -0300

    FEAT: Falta fazer as funções funcionarem e o CSS. (Obs: criar nova feat para a section de lançamentos)

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,72 @@
 
 import { ref, computed } from "vue";
 
-function incrementar() 
+function incrementar(compra, carrinho) 
 {
-  
+  compra.quantidade++
+  carrinho.totalCompra = 0
+  for (let item of carrinho.itens) 
+  {
+    item.valorTotal = item.preco * item.quantidade;
+    carrinho.totalCompra = totalCompra + item.valorTota;
+  }
 }
 
-function decrementar ()
+function decrementar(compra, carrinho)
 {
-
+  compra.quantidade--
+  if (compra.quantidade === 0) 
+  {
+    itens.splice(index, 1)
+  }
 }
 
-function totalCompra()
+function adicionarCarrinho(produto, carrinho)
 {
+  let novoItem = ref 
+    (
+      [
+        {
+          id: 0,
+          nome: '',
+          resumo: '',
+          preco: 0,
+          img: '',
+          quantidade: 1,
+          valorTotal: 0,
+        }
+      ]
+    )
 
+  novoItem.id = produto.id;
+  novoItem.nome = produto.nome;
+  novoItem.resumo = produto.resumo;
+  novoItem.preco = produto.preco;
+  novoItem.img = produto.img;
+  novoItem.valorTotal = produto.preco * produto.quantidade;
+
+  for (let categoria of carrinhos.itens) 
+  {
+    categoria.valorTotal = categoria.quantidade * categoria.preco;
+    if (categoria.id === produto.id)
+    {
+      categoria.quantidade++
+      break;
+    }
+  }
+  itens.push(novoItem)
+
+  total();
+}
+
+function total()
+{
+  carrinho.totalCompra = 0
+  for (let item of carrinho.itens) 
+  {
+    item.valorTotal = item.preco * item.quantidade
+    carrinho.totalCompra = totalCompra + item.valorTotal
+  }
 }
 
 const produtos = ref 
@@ -23,7 +76,7 @@ const produtos = ref
     {
       id: 1,
       nome: 'Combo Bubbaloo Morango',
-      resumo: 'Gloss 5 ml + Loção Hidratante Desodorante Corporal 200ml',
+      resumo: 'Gloss 5 ml + Loção Hidratante Desodorante Corporal 200ml.',
       resenha: 'O Combo Bubbaloo Morango traz dois itens essenciais em nosso momento de autocuidado diário: Um hidratante com fragrância irresistível para cuidar da pele e um gloss com textura deliciosa. Essa dupla vem com o Cheirinho de Bubbaloo Morango.',
       preco: 100.80,
       img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/products/B2022081209/B2022081209_CBEM_BUBBALOO-MOR_HID_CPO_200ML_GLOSS.jpg',
@@ -31,7 +84,7 @@ const produtos = ref
     {
       id: 2,
       nome: 'Combo Cereja Livre',
-      resumo: 'Body Splash 200ml + Loção Corporal 400ml',
+      resumo: 'Body Splash 200ml + Loção Corporal 400ml.',
       resenha: ' Com o Combo Cuide-se Bem Cereja Livre, você terá uma experiência única de cuidado e beleza com a fragrância suave e deliciosa da cereja. Ele combina produtos que proporcionam hidratação, frescor e uma sensação de leveza.',
       preco: 154.80,
       img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/product/B2025030422/32f7ad61-6a27-4f40-8145-a72de53a83df-bot-2025030422.jpg',
@@ -39,7 +92,7 @@ const produtos = ref
     {
       id: 3,
       nome: 'Creme Desodorante Hidratante Corporal',
-      resumo: 'Liz 100ml',
+      resumo: 'Liz 100ml.',
       resenha: 'O Creme Desodorante Hidratante Corporal Liz é o item para pele que completa sua rotina ou ocasião especial. Trazendo todo cuidado que seu corpo precisa e potencializa a fragrância feminina e leve de Liz com hidratação indescritível.',
       preco: 35.90,
       img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/product/B52723/11613660-b4d4-4f18-bada-80d29fba7593-bot-52723-liz-creme-hidratante-frontal-1.jpg',
@@ -47,7 +100,7 @@ const produtos = ref
     {
       id: 4,
       nome: 'Loção Desodorante Hidratante Corporal',
-      resumo: 'Cuide-se Bem Boa Noite 200ml',
+      resumo: 'Cuide-se Bem Boa Noite 200ml.',
       resenha: 'A Loção Desodorante Hidratante Corporal Cuide-se Bem Boa Noite é ideal para complementar a sua rotina de cuidados pessoais e te preparar para uma noite de sono tranquila. Afinal, ter um sono relaxante faz toda a diferença para cuidar do corpo e da mente.',
       preco: 49.90,
       img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/products/B82034/82034.jpg',
@@ -55,7 +108,7 @@ const produtos = ref
     {
       id: 5,
       nome: 'Combo Cuide-se Bem Feira Cuidados Faciais',
-      resumo: 'Gel de Limpeza Tangerina 150g + Hidratante Cremoso Banana 80g + Máscara Facial Noturna Melancia 50g',
+      resumo: 'Gel de Limpeza Tangerina 150g + Hidratante Cremoso Banana 80g + Máscara Facial Noturna Melancia 50g.',
       resenha: 'Com Cuide-se Bem Feira, a feira é diferente, está em toda parte, vai com você aonde você for, no dia e na hora que você quiser! O Combo Cuide-se Bem Feira Cuidados Faciais proporciona uma mistura de frutas tropicais para um skincare com diferentes fragrâncias, texturas e novas maneiras de se amar.',
       preco: 111.90,
       img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_1800,c_limit/e_trim/v1/imagens/product/B2024010205/1d1b5157-820c-4140-bd79-b9bbb7318c76-bot-2024010205-cuide-se-bem-feira-tangerina-gel-limpeza-facial-melancia-mascara-noturna-banana-creme-facial.jpg',
@@ -63,7 +116,7 @@ const produtos = ref
     {
       id: 6,
       nome: 'Desodorante Colônia',
-      resumo: 'Glamour Secrets Black 75ml',
+      resumo: 'Glamour Secrets Black 75ml.',
       resenha: 'Combinando diferentes flores, como a Flor de Tiaré, e frutas vermelhas, como a Framboesa, Glamour Secrets Black Desodorante Colônia traz um leve toque de Baunilha impossível de se resistir.',
       preco: 174.90,
       img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/products/B74103/GLAMOUR-DES-COL-SEC-BL-75ml-V4_B74103_frontal.jpg',
@@ -71,7 +124,7 @@ const produtos = ref
     {
       id: 7,
       nome: 'Combo Boti Baby Lua',
-      resumo: 'Colônia Para Bebê 100ml + Loção Corporal Banho e Pós-Banho 200ml',
+      resumo: 'Colônia Para Bebê 100ml + Loção Corporal Banho e Pós-Banho 200ml.',
       resenha: 'Criar um ritual de cuidados noturno para o seu bebê é essencial para uma noite de sono tranquila e reparadora. Com o Combo Boti Baby Lua, você proporciona um momento de carinho e relaxamento, além de um cheirinho delicioso que acalma e aconchega.',
       preco: 154.80,
       img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4kwVklJr4na3vh4jO8G-sqk7DcyNOxQiWlg&s',
@@ -89,13 +142,15 @@ const carrinho = ref
         id: 4,
         nome: 'Loção Desodorante Hidratante Corporal',
         resumo: 'Cuide-se Bem Boa Noite 200ml',
-        resenha: 'A Loção Desodorante Hidratante Corporal Cuide-se Bem Boa Noite é ideal para complementar a sua rotina de cuidados pessoais e te preparar para uma noite de sono tranquila. Afinal, ter um sono relaxante faz toda a diferença para cuidar do corpo e da mente.',
         preco: 49.90,
         img: 'https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/products/B82034/82034.jpg',
+        quantidade: 1,
+        valorTotal: 49.90
       },
     ],
     frete: 0,
     desconto: 0,
+    totalCompra: 0,
 }
 )
 
@@ -163,6 +218,31 @@ let idSugestao = Math.random(1, 7);
 
   <main>
 
+    <section>
+      <h1>
+        Lançamentos
+      </h1>
+      <div v-for="(produto, index) in produtos" :key="produto.id">
+           <img :src="produto.img" :alt="produto.nome" width="100">
+           <h2>
+            {{ produto.nome }}
+           </h2>
+           <p>
+            {{ produto.resumo }}
+           </p>
+           <p>
+            <span>R$</span> {{ produto.preco.toFixed(2).replace(".",",") }}
+           </p>
+           <p>
+            <span class="fa-solid fa-heart"></span>
+           </p>
+           <p @click="adicionarCarrinho(produto, index)">
+            <span class="fa-solid fa-cart-shopping"></span> Comprar
+           </p>
+         </div>
+
+    </section>
+
     <section class="carrinho">
 
       <h1>
@@ -170,17 +250,41 @@ let idSugestao = Math.random(1, 7);
       </h1>
 
       <div>
-        <ul>
-          <li>
-            Nome do Produto
-          </li>
-          <li>
-            Quantidade
-          </li>
-          <li>
-            Subtotal
-          </li>
-        </ul>
+        <p>
+          Nome do Produto
+        </p>
+        <p>
+          Quantidade
+        </p>
+        <p>
+          Subtotal
+        </p>
+      </div>
+      <div v-for="(compra, index) in carrinho.itens">
+        <div>
+          <img :src="compra.img" :alt="compra.nome" width="100">
+          <div>
+            <h2>
+            {{ compra.nome }}
+           </h2>
+           <p>
+            {{ compra.resumo }}
+           </p>
+           <p>
+            <span>R$</span> {{ compra.preco.toFixed(2).replace(".",",") }}
+           </p>
+          </div>
+        </div>
+        <div>
+          <button @click="incrementar(compra, index)">+</button>
+          <p>{{ compra.quantidade }}</p>
+          <button @click="decrementar(compra, index)">-</button>
+        </div>
+        <div>
+          <p>
+            <span>R$</span> {{ compra.valorTotal.toFixed(2).replace(".",",") }}
+          </p>
+        </div>
       </div>
       <button>
         Voltar para loja
@@ -206,12 +310,28 @@ let idSugestao = Math.random(1, 7);
             Produtos:
           </li>
           <li>
-            R$
+            <span>R$</span> {{ totalCompra }}
           </li>
         </ul>
         <ul>
-
+          <li>
+            Frete:
+          </li>
+          <li>
+            Grátis
+          </li>
         </ul>
+        <ul>
+          <li>
+            Total:
+          </li>
+          <li>
+            {{ (carrinho.totalCompra + carrinho.frete - carrinho.desconto).toFixed(2).replace(".",",") }}
+          </li>
+        </ul>
+        <button>
+          Ir para o pagamento
+        </button>
       </div>
     </div>
 
