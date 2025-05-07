6da0f1125e9461f638d8ee28aef4b993ab0b79af
analaura3s <analauras.ifc.info3@gmail.com>
2025-05-07 15:05:35 -0300

    FEAT: Footer quase pronto #5

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,45 @@ import { ref, computed } from "vue";
 
     </div>
   </header>
-  
+
+<footer>
+
+<ul class="contato">
+
+<li>Contato: 41 3406-8393</li>
+
+<li>Seg. à sex., 8h às 20h - Sáb., 9h às 18h</li>
+
+</ul>
+
+<ul class="icones">
+
+<li><i class="fa-brands fa-square-facebook"></i></li>
+
+<li><i class="fa-brands fa-square-instagram"></i></li>
+
+<li><i class="fa-brands fa-square-twitter"></i></li>
+
+<li><i class="fa-brands fa-square-youtube"></i></li>
+
+<li><i class="fa-brands fa-square-whatsapp"></i></li>
+
+<li><i class="fa-solid fa-square-phone"></i></li>
+
+</ul>
+
+<ul class="cartoes">
+
+<li><i class="fa-brands fa-cc-visa"></i></li>
+
+<li><i class="fa-brands fa-cc-mastercard"></i></li>
+
+<li><i class="fa-brands fa-pix"></i></li>
+
+</ul>
+
+</footer>
+
 </template>
 
 <style src="./assets/reset.css">
